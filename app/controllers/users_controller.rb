107b1af9b5a2a2fdf19866483ea0d6f60cb829da
Virginia Dooley <vcdooley@gmail.com>
2019-01-22 20:48:14 +0000

    fixed the routes for get /users/account and post /users/signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  # get "/users" do
-  #   erb :"/users/index.html"
-  # end
-
   #Create
   # GET: /users/signup
   get "/users/signup" do
@@ -12,28 +7,51 @@ class UsersController < ApplicationController
   end
 
   # POST: /users/signup
-  post "users/signup" do
+  post "/users/signup" do
   # searches for User params and authenticates
-    @user = User.find_by(email: params[:email], password: params[:password])
+    # user = User.find_by(username: params[:username], password: params[:password])
+    # session[:user_id] = user.id
+    # if user && user.authenticate(params[:password])
+	  #   session[:user_id] = user.id
+	  #   redirect "users/login"
+	  # else
+	  #   redirect "users/failure"
+	  # end
+    #   redirect "users/account"
 
-    if @user && @user.authenticate(params[:password])
+    if params[:username] == "" || params[:password] == ""
+      redirect 'users/failure.html'
+    else
+      user = User.create(username: params[:username], password: params[:password])
+      session[:user_id] = user.id
+      redirect '/users/account'
+    end
+  end
+
+  # GET: /users/account/5
+  get "/users/account" do
+    user = User.find(session[:user_id])
+    erb :"/users/account.html"
+  end
+
+  get "/users/login" do
+    erb :login
+  end
+
+  post "/users/login" do
+    user = User.find_by(:username => params[:username])
+
+	  if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
-	    redirect "users/login"
+	    redirect "users/account"
 	  else
 	    redirect "users/failure"
 	  end
-      redirect "users/account"
-  end
-
-  # GET: /users/5
-  get "/users/:id" do
-    @user = User.find(session[:user_id])
-    erb :"/users/account.html"
   end
 
   # GET: /users/5/edit
   get "/users/:id/edit" do
-    erb :"/users/edit.html"
+    erb :"/users/edit"
   end
 
   # PATCH: /users/5

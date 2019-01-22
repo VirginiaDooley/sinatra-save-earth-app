ec07f19a75aa84d31e07843ae4853b49e01afeb8
Virginia Dooley <vcdooley@gmail.com>
2019-01-22 10:03:44 +0000

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190122091059) do
+ActiveRecord::Schema.define(version: 20190122095526) do
 
   create_table "actions", force: :cascade do |t|
     t.string   "action_name"
     t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|

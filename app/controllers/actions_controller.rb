e442c209cc8cebbbfd9cd770bd9e0ef0ef5de4e5
Virginia Dooley <vcdooley@gmail.com>
2019-01-23 16:59:19 +0000

    fix actions create route

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,41 +1,36 @@
 class ActionsController < ApplicationController
 
-  # GET: /actions
   get "/actions" do
-    @actions = Actions.all
     erb :"/actions/index.html"
   end
 
-  # GET: /actions/new
   get "/actions/new" do
     @users = User.all
     erb :"/actions/new.html"
   end
 
-  # POST: /actions
-  post "/actions" do
+  post "/actions/create" do
+    binding.pry
     action = Action.create(action_name: params[:action_name], status: params[:status])
     session[:action_id] = action.id
-    redirect "actions/#{@action.id}"
+
+    redirect "/actions/#{action.id}"
   end
 
-  # GET: /actions/5
   get "/actions/:id" do
+    action = Action.find(params[:id])
     erb :"/actions/show.html"
   end
 
-  # GET: /actions/5/edit
   get "/actions/:id/edit" do
     erb :"/actions/edit.html"
   end
 
-  # PATCH: /actions/5
   patch "/actions/:id" do
-    redirect "/actions/:id"
+    redirect '/actions/:id'
   end
 
-  # DELETE: /actions/5/delete
   delete "/actions/:id/delete" do
-    redirect "/actions"
+    redirect '/actions'
   end
 end
